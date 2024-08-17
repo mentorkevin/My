1c59e2da21928b931a120d2116d8e3d0a86be4d4
MWTU-TECH-AI <mistarkelvin@outlook.com>
2024-08-17 17:06:55 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,25 +2,33 @@
 #Don't copy or modify code Read License First
 #Subscribe me on YouTube www.youtube.com/c/Mwtuofficial
 clear
-echo "\e[0m\n" 
-echo -e "CODED BY MWTU\e[0m\n"
-echo -e "                                __      __     ______    __   __\e[0m\n" 
-echo -e "\e[1;92m/'\_/`\    \e[1;94m/\ \  __/\ \   \e[1;97m/\__  _\  \e[1;98m/\ \ /\ \\e[0m\n" 
-echo -e "\e[1;92m/\      \   \e[1;94m\ \ \ \ \ \ \  \e[1;97m\/_/\ \/  \e[1;98m\ \ \  \ \\e[0m\n"
-echo -e "\e[1;92m\ \ \__\ \   \e[1;94m\ \ \ \ \ \ \    \e[1;97m\ \ \   \e[1;98m\ \ \  \ \\e[0m\n"
-echo -e "\e[1;92m \ \ \_/\ \   \e[1;94m\ \ \_/ \_\ \    \e[1;97m\ \ \   \e[1;98m\ \ \__\ \\e[0m\n"
-echo -e "\e[1;92m\ \_\  \ \_\   \e[1;94m\ `\___x___/     \e[1;97m\ \_\   \e[1;98m\ \_____\\\e[0m\n"
-echo -e "\e[1;92m\/_/    \/_/    \e[1;94m'\/__//__/       \e[1;97m\/_/    \e[1;98m\/_____/\e[0m\n" 
-echo -e "\e[1;92m [+] YouTube: Mwtuofficial\e[1;98m"
-echo -e "\e[1;98m [+] Github: Mwtutech\e[1;99m" 
-echo -e "\e[1;91m [+] Instagram: @ mwtuofficial\e[1;96m"
-echo "----------------------------"
-echo "
-+-+-+-+-+-+-+-+
-|W|e|l|c|o|m|e|
-+-+-+-+-+-+-+-+"
+printf "\e[0m\n"
+printf "\e[0m\e[93m .___  ___. ____    __    ____ .___________. __    __ \e[0m\n"
+printf "\e[0m\e[93m |   \/   | \   \  /  \  /   / |           ||  |  |  |  \e[0m\n"
+printf "\e[0m\e[93m |  \  /  |  \   \/    \/   /  `---|  |----`|  |  |  |  \e[0m\n"
+printf "\e[0m\e[93m |  |\/|  |   \            /       |  |     |  |  |  |  \e[0m\n"
+printf "\e[0m\e[93m |  |  |  |    \    /\    /        |  |     |  `--'  |  \e[0m\n"
+printf "\e[0m\e[93m |__|  |__|     \__/  \__/         |__|      \______/   \e[0m\n"
+                                                       
 
-echo "\e[0m\n"
+   ______    _______  _______  __    ______  __       ___       __      
+  /  __  \  |   ____||   ____||  |  /      ||  |     /   \     |  |     
+ |  |  |  | |  |__   |  |__   |  | |  ,----'|  |    /  ^  \    |  |     
+ |  |  |  | |   __|  |   __|  |  | |  |     |  |   /  /_\  \   |  |     
+ |  `--'  | |  |     |  |     |  | |  `----.|  |  /  _____  \  |  `----.
+  \______/  |__|     |__|     |__|  \______||__| /__/     \__\ |_______|
+                                                                        
+
+
+printf "\e[0m\e[93m _  _ ___ _   _    ___  _  _ ___ ___ _  _ ___  ___
+printf "\e[0m\e[93m |\ | |__  \_/    |___] |__|  |  |__ |__| |__ |__/ \e[0m\n"
+printf "\e[0m\e[93m | \| |__ _/ \_   |    _|  | _|_ ___||  |_|__ | \_ \e[0m\e[1;46m[V 1.0]\e[0m\n"
+printf "\e[0m\n"
+printf "    \e[0m\e[1;44m Advanced Phishing Tool with 30+ Templates\e[0m\n"
+printf "\e[0m\n"
+printf "\e[0m\e[91m    [\e[0m\e[96m BY \e[0m\e[91m: \e[0m\e[96mhttps://github.com/mwtutech \e[0m\e[91m] \e[0m\n"
+printf "\e[0m\n"
+
 read -p " Does you want to install Mwtu Shell in Termux?(Yes/No) : " input
 
 if [[ $input == Yes || $input == yes || $input == y || $input == Y ]]; then
